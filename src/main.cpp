d8eadd88c5d11422da8ccd9b8f6e4a6e21ca9e25
Software-enthusiast1 <christopher.d.curtis1@gmail.com>
2026-08-22 13:12:55 -0700

    fps counter, movement in delta time

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,30 @@ int main() {
 	int currentMonitor = GetCurrentMonitor();
 	int refreshRate = GetMonitorRefreshRate(currentMonitor);
 	SetTargetFPS(refreshRate);
-
 	// Renders at standard 1080p, sizes up/down to screen
 	const float FIXED_WIDTH = 1920;
 	const float FIXED_HEIGHT = 1080;
 	RenderTexture2D canvas = LoadRenderTexture((int)FIXED_WIDTH, (int)FIXED_HEIGHT);
 	SetTextureFilter(canvas.texture, TEXTURE_FILTER_BILINEAR);
 
+	// Variables for the game
+	Vector2 position = { 200, 200 };
+	int speed = 200;
+
 	while (!WindowShouldClose()) {
+		int fps = GetFPS();
+		float dt = GetFrameTime();
+		
+		// Game logic
+		if (IsKeyDown(KEY_RIGHT)) position.x += speed * dt;
+		if (IsKeyDown(KEY_LEFT))  position.x -= speed * dt;
+		if (IsKeyDown(KEY_DOWN))  position.y += speed * dt;
+		if (IsKeyDown(KEY_UP))    position.y -= speed * dt;
+
 		BeginTextureMode(canvas);
 			ClearBackground(BLACK);
-			DrawLine(0, 0, 1920, 1080, RED);
-			DrawLine(0, 1080, 1920, 0, RED);
-			DrawLine(0, 0, 1920, 0, BLUE);
-			DrawLine(0, 1080, 1920, 1080, BLUE);
-			DrawLine(0, 0, 0, 1080, BLUE);
-			DrawLine(1920, 0, 1920, 1080, BLUE);
-			DrawText("Fixed Coordinate system", 50, 50, 30, RAYWHITE);
-			DrawText("0,0", 0, 0, 30, RAYWHITE);
+			DrawLine(0, 0, position.x, position.y, RED);
+			DrawText(TextFormat("FPS: %i", fps), 0, 0, 30, RAYWHITE);
 		EndTextureMode();
 
 		BeginDrawing();
